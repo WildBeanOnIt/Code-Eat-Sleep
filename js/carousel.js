d8d094bf878d94ced160e8b96884e5f186f07808
Julio Aguilera Jr <julioaguilera337@gmail.com>
2023-09-26 21:17:09 -0500

    made the carousel work.

--- js/carousel.js
+++ js/carousel.js
@@ -5,20 +5,27 @@ let current = Math.floor(Math.random() * slides.length);
 let next = current < slides.length - 1 ? current +1 : 0;
 let prev = current > 0 ? current - 1 : slides.length - 1;
 
+const update = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active", "prev", "next");
+    })
+    slides[current].classList.add("active");
+    slides[prev].classList.add("prev");
+    slides[next].classList.add("next");
+};
 
-const dummySlides = [
-    // slide 0
-    // slide 1
-    // slide 2
-    // slide 3
-]
+const goToNum = (number) => {
+    current = number;
+    next = current < slides.length - 1 ? current +1 : 0;
+    prev = current > 0 ? current - 1 : slides.length - 1;
+    update();
+}
 
-//? create an eventListener for prev/next
+const goToNext = () => current < slides.length - 1 ? goToNum(current + 1) : goToNum(0);
+const goToPrev = () => current > 0 ? goToNum(current - 1) : goToNum(slides.length -1);
 
+for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', () => i === 0 ? goToPrev() : goToNext());
+}
 
-//? -- decide how to call the prev/next
-//? --update variables
-//? --- [current] = newIndex
-//? --- [next] current + 1 : 0
-//? --- [prev] current + 1 : length -1
-//? -- update the css classes
+update();
